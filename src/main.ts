43a511593f326557743cb93bb820363505b44875
PhuocTaii <tai.ngo1773@gmail.com>
2024-11-11 21:45:43 +0700

    commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',  // Cho phép tất cả các domain
+    methods: 'GET,POST,PUT,DELETE',  // Các phương thức được phép
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
